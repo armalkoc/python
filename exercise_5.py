567b21babe4270ce78fe6232e413d5e8debf2ed9
amalkoc <armin.malkoc@insparx.com>
2026-05-19 13:27:32 +0200

    exercise_5 done

---
+++ exercise_5.py
@@ -0,0 +1,48 @@
+def calculator(number1, number2, operation):
+    match operation:
+        case "plus":
+            print(number1 + number2)
+        case "minus":
+            print(number1 - number2)
+        case "multiply":
+            print(number1 * number2)
+        case "divide":
+            print(number1 / number2)
+
+num_of_calculations = 0
+while True:
+    num1 = input("Dear user please enter the number1:\n")
+
+    # check user entry is number1
+    if num1 == 'exit':
+        print("Your input is not number !")
+        print(f"You did {num_of_calculations} calculations")
+        break
+
+    num2 = input("Dear user please enter the number2:\n")
+
+    # check if user entry is valid number2
+    if num2 == 'exit':
+        print("Your input is not number !")
+        print("You did {num_of_calculations} calculations")
+        break
+
+    # check if these entries are valid numbers not any other kind of entry
+    valid_numbers = num1.isnumeric() or num2.isnumeric()
+
+    operation = input("Dear user, please enter an operation you want to perform (plus, minus, divide or multipy):\n")
+    valid_operation = operation == "plus" or operation == "minus" or operation == "divide" or operation == "multiply"
+
+    if not valid_numbers:
+        print("Sorry, only numbers are allowed")
+    elif not valid_operation:
+        print("Sorry, only +,-,*,/ are allowed")
+    else:
+        calculator(int(num1), int(num2), operation)
+        num_of_calculations += 1
+
+
+
+
+
+
